99c6284093f003df68752454b953be2597a06c18
LuisSanguinetti <luis.eduardo.sanguinetti@gmail.com>
2025-08-27 22:02:43 -0300

    code with proffesors error

---
+++ Practico_1_AVL/TAD/avl_Profesor.cpp
@@ -0,0 +1,161 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+struct NodoAVL
+{
+    int dato;
+    NodoAVL *izq;
+    NodoAVL *der;
+    int fVal;
+
+    NodoAVL(int x): dato(x), izq(NULL), der(NULL), fVal(0) {}
+};
+
+struct RepresentacionAVL
+{
+  NodoAVL *raiz;
+  int cantElementos;
+
+  RepresentacionAVL(): raiz(NULL), cantElementos(0) {}
+};
+typedef RepresentacionAVL *AVL;
+
+AVL crearAVL()
+{
+    return new RepresentacionAVL();
+}
+
+// inserta el nodo en el AVL utilizando recursion
+bool insertarAVLRec(NodoAVL *&pa, int x, bool &varioAltura)
+{
+    bool insertado;
+    if (pa == NULL)
+    {
+        pa = new NodoAVL(x);
+        varioAltura = true;
+        return true;
+    }
+    if (x < pa->dato)
+    {
+        insertado = insertarAVLRec(pa->izq, x, varioAltura);
+        if (varioAltura)
+        {
+            switch (pa->fVal)
+            {
+                case 1:
+                    pa->fVal = 0;
+                    varioAltura = false;
+                    break;
+                case 0:
+                    pa->fVal = -1;
+                    break;
+                case -1:
+                    NodoAVL *p1 = pa->izq;
+                    if(p1->fVal == -1){
+                        pa->izq = p1->der;
+                        p1->der = pa;
+                        pa->fVal = 0;
+                        pa = p1;
+                    } else {
+                        NodoAVL *p2 = p1->der;
+                        p1->der = p2->izq;
+                        p2->izq = p1;
+                        pa->izq = p2->der;
+                        p2->der = pa;
+                        pa->fVal = p2->fVal == -1 ? 1 : 0;
+                        p1->fVal = p2->fVal == 1 ? -1 : 0;
+                        pa = p2;
+                    }
+                    pa->fVal = 0;
+                    varioAltura = false;
+                    break;
+            }
+            varioAltura = false;
+        }
+        return insertado;
+    }
+    else if (x > pa->dato)
+    {
+        insertado = insertarAVLRec(pa->der, x, varioAltura);
+        if (varioAltura)
+        {
+            switch (pa->fVal)
+            {
+                case -1:
+                    pa->fVal = 0;
+                    varioAltura = false;
+                    break;
+                case 0:
+                    pa->fVal = 1;
+                    break;
+                case 1:
+                    NodoAVL *p1 = pa->der;
+                    if(p1->fVal == 1){
+                        pa->der = p1->izq;
+                        p1->izq = pa;
+                        pa->fVal = 0;
+                        pa = p1;
+                    } else {
+                        NodoAVL *p2 = p1->izq;
+                        p1->izq = p2->der;
+                        p2->der = p1;
+                        pa->der = p2->izq;
+                        p2->izq = pa;
+                        pa->fVal = p2->fVal == 1 ? -1 : 0;
+                        p1->fVal = p2->fVal == -1 ? 1 : 0;
+                        pa = p2;
+                        return insertado;
+                    }
+                    pa->fVal = 0;
+                    varioAltura = false;
+                    break;
+            }
+            varioAltura = false;
+        }
+        return insertado;
+    } else return false;
+}
+
+// Metodo principal para insertar en el AVL
+void insertarAVL(AVL a, int e)
+{
+    bool varioAltura = false;
+    bool insertado = insertarAVLRec(a->raiz, e, varioAltura);
+    if (insertado)
+    {
+        a->cantElementos++;
+    }
+}
+
+// Imprime el AVL en orden creciente de forma recursiva
+void inorderAVLRec(NodoAVL* a)
+{
+    if (a != NULL)
+    {
+        inorderAVLRec(a->izq);
+        cout << a->dato << endl;
+        inorderAVLRec(a->der);
+    }
+}
+
+// Metodo principal para imprimir el AVL en orden
+void inorderAVL(AVL a)
+{
+    inorderAVLRec(a->raiz);
+}
+
+int main()
+{
+    AVL a = crearAVL();
+    int cant;
+    cin >> cant;
+    for (int i = 0; i < cant; i++)
+    {
+        int x;
+        cin >> x;
+        insertarAVL(a, x);
+    }
+    inorderAVL(a);
+    return 0;
+}
--- Practico_1_AVL/main.cpp
+++ Practico_1_AVL/main.cpp
@@ -3,8 +3,30 @@
 #include <iostream>
 #include <limits>
 #include "TAD/avl.cpp"
+//#include "TAD/avl_Profesor.cpp"
 using namespace std;
 
+// codigo profesor avl
+int main()
+{
+    int n;
+    if (!(cin >> n)) return 0;
+
+    AVL a = crearAVL();
+    for (int i = 0; i < n; ++i) {
+        int x; 
+        cin >> x;
+        insertarAVL(a, x);
+    }
+
+    // imprimir in-order, un número por línea
+    inorderAVLRec(a->raiz); // cambia la línea de impresión si querés \n (ver nota)
+    cout << '\n';
+    return 0;
+}
+
+//mi codigo para el avl
+/*
 int main()
 {
     int n;
@@ -22,3 +44,5 @@ int main()
     cout << '\n';
     return 0;
 }
+
+*/
